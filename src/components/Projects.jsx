555c1487d5eaae06b0bdc6e4d590ab8174933f51
Jonathan Zamora <jonathan.zamoraa@gmail.com>
2023-02-16 00:00:35 -0500

    mejora en la seccion de projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -14,16 +14,16 @@ export const Projects = () => {
                 <p className='py-3 leading-8 text-gray-800 dark:text-slate-300'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum doloremque quisquam fugiat! A repellendus eum eos et, perspiciatis consequuntur, architecto dolores provident aut obcaecati quibusdam dolorem. Libero accusantium quas deserunt. </p>
             </div>
 
-            <div>
+            <div className='flex justify-center w-full bg-red-200'> 
                 <div className='grid p-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 bg-slate-700 gap-8' >
                     {
                         projects.map((item, index) => {
 
                             const {title, img, link ,tools} = item;
 
-                            return <div key={index} className='group bg-red-200 bg-cover border-2 relative overflow-hidden'>
+                            return <div key={index} className='group max-w-sm bg-red-200 bg-cover border-2 relative overflow-hidden'>
 
-                                <div className='group-hover:bg-black/70 w-full h-full absolute z-20 transition-all duration-300'></div>
+                                <div className='group-hover:bg-black/80 w-full h-full absolute z-20 transition-all duration-300'></div>
                                 <img src={img} alt="" className='w-full group-hover:scale-125 transition-all duration-500s'/>
 
                                 <div className='absolute -bottom-full w-full left-0 p-3 group-hover:bottom-0 transition-all duration-500 z-30 '>
